c66d5afcb85da3c9c182ff54b00cda323838f5c2
Francisco Falcon <fdfalcon@gmail.com>
2012-05-28 02:56:21 -0300

    modified:   brainfword/brainfword.cpp :Tabs->spaces

--- brainfword/brainfword.cpp
+++ brainfword/brainfword.cpp
@@ -32,12 +32,12 @@
 
 #include "stdafx.h"
 #ifdef _WIN32
-	#include <windows.h>
+    #include <windows.h>
 #endif
 
 #ifdef __linux__
-	#include <stdlib.h>
-	#include <string.h>
+    #include <stdlib.h>
+    #include <string.h>
 #endif
 
 
@@ -52,195 +52,195 @@ unsigned long pc = 0;
 
 
 void clean_memory(){
-	memset(memory, 0, MEM_SIZE);
+    memset(memory, 0, MEM_SIZE);
 }
 
 
 void reset_pointer(){
-	pointer = 0;
+    pointer = 0;
 }
 
 
 void free_memory(){
-	if (memory){
-		free(memory);
-	}
+    if (memory){
+        free(memory);
+    }
 
-	if (program){
-		free(program);
-	}
+    if (program){
+        free(program);
+    }
 }
 
 
 int is_balanced(unsigned char *code, unsigned long codesize){
-	unsigned long counter = 0;
-	unsigned char c;
-	int balance = 0;
+    unsigned long counter = 0;
+    unsigned char c;
+    int balance = 0;
 
-	while (counter < codesize){
-		c = code[counter++];
-		switch (c){
-			case '[':
-				balance++;
-				break;
-			case ']':
-				if (!balance){
-					return 0;
-				}
-				else{
-					balance--;
-				}
-				break;
-		}
-	}
-	return !balance;
+    while (counter < codesize){
+        c = code[counter++];
+        switch (c){
+            case '[':
+                balance++;
+                break;
+            case ']':
+                if (!balance){
+                    return 0;
+                }
+                else{
+                    balance--;
+                }
+                break;
+        }
+    }
+    return !balance;
 
 }
 
 
 void load_program(char *filename){
-	FILE *pFile;
+    FILE *pFile;
 
 #ifdef _WIN32
-	//fopen_s returns 0 if successful
-	if (fopen_s(&pFile, filename, "rb")){
-		printf("ERROR: could not open file %s, exiting...\n", filename);
-		free_memory();
-		exit(EXIT_FAILURE);
-	}
+    //fopen_s returns 0 if successful
+    if (fopen_s(&pFile, filename, "rb")){
+        printf("ERROR: could not open file %s, exiting...\n", filename);
+        free_memory();
+        exit(EXIT_FAILURE);
+    }
 #endif
 
 #ifdef __linux__
-	pFile = fopen(filename, "rb");
-	if (!pFile){
-		printf("ERROR: could not open file %s, exiting...\n", filename);
-		free_memory();
-		exit(EXIT_FAILURE);
-	}
+    pFile = fopen(filename, "rb");
+    if (!pFile){
+        printf("ERROR: could not open file %s, exiting...\n", filename);
+        free_memory();
+        exit(EXIT_FAILURE);
+    }
 #endif
 
-	/* Calculate file size */
-	fseek(pFile, 0, SEEK_END);						//seek to end of file
-	program_size = ftell(pFile);					//get current file pointer
-	fseek(pFile, 0, SEEK_SET);						//seek back to beginning of file
+    /* Calculate file size */
+    fseek(pFile, 0, SEEK_END);                        //seek to end of file
+    program_size = ftell(pFile);                    //get current file pointer
+    fseek(pFile, 0, SEEK_SET);                        //seek back to beginning of file
 
-	program = (unsigned char*)malloc(program_size);
-	if (!program){
-		fclose(pFile);
-		printf("ERROR: Could not allocate %d bytes to load the program from file %s.\n", program_size, filename);
-		free_memory();
-		exit(EXIT_FAILURE);
-	}
-	fread(program, 1, program_size, pFile);
+    program = (unsigned char*)malloc(program_size);
+    if (!program){
+        fclose(pFile);
+        printf("ERROR: Could not allocate %d bytes to load the program from file %s.\n", program_size, filename);
+        free_memory();
+        exit(EXIT_FAILURE);
+    }
+    fread(program, 1, program_size, pFile);
 
-	fclose(pFile);
-	pc = 0;						//Reset the program counter
+    fclose(pFile);
+    pc = 0;                        //Reset the program counter
 }
 
 
 void execute_program(){
-	unsigned char instr;
+    unsigned char instr;
 
-	while (pc < program_size){
-		instr = program[pc++];
-		switch(instr){
-			case '>':
-					//Avoid setting the pointer after the end of the memory buffer
-					if (pointer < MEM_SIZE - 1){
-						pointer++;
-					}
-					break;
-			case '<':
-					//Avoid setting the pointer before the beginning of the memory buffer
-					if (pointer > 0){
-						pointer--;
-					}
-					break;
-			case '+':
-					memory[pointer]++;
-					break;
-			case '-':
-					memory[pointer]--;
-					break;
-			case '.':
-					printf("%c", memory[pointer]);
-					break;
-			case ',':
-					fread(&memory[pointer], 1, 1, stdin);
-					break;
-			case '[':
-					//When memory[pointer] == 0, skip to the matching ']'
-					if (!memory[pointer]){
-						unsigned int balance = 0;
-						unsigned char temp_instr;
-						int found = 0;
-						while ((pc < program_size) && (!found)){
-							temp_instr = program[pc++];
-							switch (temp_instr){
-								case '[':
-									balance++;
-									break;
-								case ']':
-									if (balance){
-										balance--;
-									}
-									else{
-										found = 1;
-									}
-									break;
+    while (pc < program_size){
+        instr = program[pc++];
+        switch(instr){
+            case '>':
+                    //Avoid setting the pointer after the end of the memory buffer
+                    if (pointer < MEM_SIZE - 1){
+                        pointer++;
+                    }
+                    break;
+            case '<':
+                    //Avoid setting the pointer before the beginning of the memory buffer
+                    if (pointer > 0){
+                        pointer--;
+                    }
+                    break;
+            case '+':
+                    memory[pointer]++;
+                    break;
+            case '-':
+                    memory[pointer]--;
+                    break;
+            case '.':
+                    printf("%c", memory[pointer]);
+                    break;
+            case ',':
+                    fread(&memory[pointer], 1, 1, stdin);
+                    break;
+            case '[':
+                    //When memory[pointer] == 0, skip to the matching ']'
+                    if (!memory[pointer]){
+                        unsigned int balance = 0;
+                        unsigned char temp_instr;
+                        int found = 0;
+                        while ((pc < program_size) && (!found)){
+                            temp_instr = program[pc++];
+                            switch (temp_instr){
+                                case '[':
+                                    balance++;
+                                    break;
+                                case ']':
+                                    if (balance){
+                                        balance--;
+                                    }
+                                    else{
+                                        found = 1;
+                                    }
+                                    break;
 
-							}
-						}
-					}
-					break;
-			case ']':
-					//If memory[pointer] != 0, repeat the loop by jumping back to the matching '['
-					if (memory[pointer]){
-						unsigned int balance = 0;
-						unsigned char temp_instr;
-						int found = 0;
-						pc--;				//Fetching of 'instr' made pc++ at the very beginning of the while loop
-						while ((pc > 0) && (!found)){
-							temp_instr = program[--pc];
-							switch (temp_instr){
-								case ']':
-									balance++;
-									break;
-								case '[':
-									if (balance){
-										balance--;
-									}
-									else{
-										found = 1;
-									}
-							}
-						}
-					}
-					break;
+                            }
+                        }
+                    }
+                    break;
+            case ']':
+                    //If memory[pointer] != 0, repeat the loop by jumping back to the matching '['
+                    if (memory[pointer]){
+                        unsigned int balance = 0;
+                        unsigned char temp_instr;
+                        int found = 0;
+                        pc--;                //Fetching of 'instr' made pc++ at the very beginning of the while loop
+                        while ((pc > 0) && (!found)){
+                            temp_instr = program[--pc];
+                            switch (temp_instr){
+                                case ']':
+                                    balance++;
+                                    break;
+                                case '[':
+                                    if (balance){
+                                        balance--;
+                                    }
+                                    else{
+                                        found = 1;
+                                    }
+                            }
+                        }
+                    }
+                    break;
 
-		}
-	}
+        }
+    }
 }
 
 
 int main(int argc, char* argv[])
 {
-	if (argc < 2){
-		printf("\nUsage: %s <brainfuck-code.txt>\n", argv[0]);
-		free_memory();
-		exit(EXIT_FAILURE);
-	}
-	clean_memory();
-	reset_pointer();
-	
-	load_program(argv[1]);
-	if (!is_balanced(program, program_size)){
-		printf("ERROR: Unbalanced brackets found in the supplied Brainfuck code.\n");
-		free_memory();
-		exit(EXIT_FAILURE);
-	}
-	execute_program();
-	free_memory();
-	return 0;
+    if (argc < 2){
+        printf("\nUsage: %s <brainfuck-code.txt>\n", argv[0]);
+        free_memory();
+        exit(EXIT_FAILURE);
+    }
+    clean_memory();
+    reset_pointer();
+    
+    load_program(argv[1]);
+    if (!is_balanced(program, program_size)){
+        printf("ERROR: Unbalanced brackets found in the supplied Brainfuck code.\n");
+        free_memory();
+        exit(EXIT_FAILURE);
+    }
+    execute_program();
+    free_memory();
+    return 0;
 }
 
